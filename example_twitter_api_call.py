407e71ebb5c18083bc2b3ff191d16d5d1eb1824a
Nathaniel Taylor <nateisgood@gmail.com>
2020-02-09 10:56:26 -0500

    finished working example call, preparing to merge

--- example_twitter_api_call.py
+++ example_twitter_api_call.py
@@ -5,4 +5,26 @@ Created on Sun Feb  9 10:09:44 2020
 
 @author: nate
 """
+import twitter
+import json
 
+def load_creds():
+    with open('creds.json', 'r') as file:
+        creds = json.load(file)
+    
+    return creds
+
+creds = load_creds()
+
+api = twitter.Api(**creds)
+
+results = api.GetSearch('Bojack Horseman')
+
+
+#%%
+obj = {
+    'consumer_key': 'lEU4diypFEoBnbUg6tVoPOVBz',
+    'consumer_secret': 'aE2x6GYgZFPOJ19Boy6H7gGsr6RWwhhnifKgMtNIqlcGlpz17N',
+    'access_token_key': '1223115410486128641-6yyrlupiyZP1WR7nGvSe3eD5jeEXug',
+    'access_token_secret': 'Gl6yBicVf7hpKfDuidbTfY906h4fSne9O6OzLOeCamHsd'   
+}
