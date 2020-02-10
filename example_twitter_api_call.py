5c30c0e6495e5be2b2d561bc357d28c947425bb3
Francisco <franciscothebrady@gmail.com>
2020-02-10 01:08:49 -0500

    tried and failed to use the API

--- example_twitter_api_call.py
+++ example_twitter_api_call.py
@@ -16,6 +16,10 @@ def load_creds():
 
 creds = load_creds()
 
-api = twitter.Api(**creds)
+api = twitter.Api(consumer_key=creds['consumer_key'],
+                  consumer_secret=creds['consumer_secret'],
+                  access_token_key=creds['access_token_key'],
+                  access_token_secret=creds['access_token_secret'])
+# I get this error when i try this: TypeError: 'module' object is not callable
 
 results = api.GetSearch('Bojack Horseman')
